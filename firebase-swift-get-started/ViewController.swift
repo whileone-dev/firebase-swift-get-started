426e77c25ec758f9db1bc0752d4af5838e7b3549
mq9117 <mq9117@gmail.com>
2021-03-20 10:23:11 +0100

    Initial Commit

---
+++ firebase-swift-get-started/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase-swift-get-started
+//
+//  Created by Mark Quinn on 20.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
